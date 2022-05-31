476bafb51e36f7f38a981d735f3e2aeabcf7540b
Mike Solomon <mikesol@hey.com>
2022-05-31 12:31:29 -0700

    Add the ability to set nicknames
    Users can type /nickname <name> to change their name to something
    else.

--- index.js
+++ index.js
@@ -7,23 +7,39 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
 // Whenever someone connects this gets executed
 socketIOServer.on("connection", (socket) => {
+  let nickname = "user" + getRandomInt(1, 10000);
+
   console.log("A user connected!");
   console.log("Number of active clients:", socketIOServer.engine.clientsCount);
+  socketIOServer.emit("chat message", `${nickname} joined the chat`);
 
-  socket.on("new user", (data) => {
-    socket.userId = data;
-    console.log(`data: ${data}`);
+  socket.on("change nickname", (newNickname) => {
+    console.log(
+      `User ${nickname} requested nickname change to: ${newNickname}`
+    );
+    socketIOServer.emit(
+      "chat message",
+      `${nickname} changed their name to ${newNickname}`
+    );
+    nickname = newNickname;
   });
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected");
+    console.log(`${nickname} left the chat`);
+    socketIOServer.emit("chat message", `${nickname} left the chat`);
   });
 
   socket.on("chat message", (msg) => {
     console.log("Received message:", msg);
-    socketIOServer.emit("chat message", msg);
+    socketIOServer.emit("chat message", `${nickname}: ${msg}`);
   });
 });
 
